585c1fe626a429b43f1f9c2b81ad7f90d531a48d
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2022-06-18 21:27:14 +0530

    Added XTerm to terminal

--- src/components/Terminal.jsx
+++ src/components/Terminal.jsx
@@ -1,12 +1,19 @@
-import { Component, useState } from "react";
+import { Component, useState }  from "react";
+import React from "react";
+import XTerm from './XTerm.tsx'
 
 class Terminal extends Component{
     constructor(props)
     {
         super(props);
-        this.state={previous:[],command:""};
+        // this.state={previous:[],command:""};
+        this.xtermRef = React.createRef();
         this.handleSubmit = this.handleSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+    }
+    componentDidMount()
+    {
+        this.xtermRef.current.terminal.writeln('Hello, World!');
     }
     handleSubmit(event)
     {
@@ -27,26 +34,28 @@ class Terminal extends Component{
     }
     render(){
         return(
-            <div className="container-fluid overflow-auto">
-                <div className="row text-white">
-                {
-                    this.state.previous.map((item,index,array)=>{
-                        return (<span className="text-left">
-                            {item}
-                            </span>
-                        );
-                    })
-                }
-                <div className="text-left">{"[abhigyan.ghosh@researchweb.iiit.ac.in] $"}
-                    <span className="inline text-left">
-                        <form className="pl-1 inline text-left" onSubmit={this.handleSubmit}>
-                            <input type="text" onChange={this.handleChange} value={this.state.command} className="bg-dark focus:border-0 focus-visible:outline-none text-slate-200"></input>
-                            <input type="submit" hidden/>
-                        </form>
-                    </span>
-                </div>
-                </div>
-            </div>
+            // <div className="container-fluid overflow-auto">
+            //     <div className="row text-white">
+            //     {
+            //         this.state.previous.map((item,index,array)=>{
+            //             return (<span className="text-left">
+            //                 {item}
+            //                 </span>
+            //             );
+            //         })
+            //     }
+            //     <div className="text-left">{"[abhigyan.ghosh@researchweb.iiit.ac.in] $"}
+            //         <span className="inline text-left">
+            //             <form className="pl-1 inline text-left" onSubmit={this.handleSubmit}>
+            //                 <input type="text" onChange={this.handleChange} value={this.state.command} className="bg-dark focus:border-0 focus-visible:outline-none text-slate-200"></input>
+            //                 <input type="submit" hidden/>
+            //             </form>
+            //         </span>
+            //     </div>
+            //     </div>
+            // </div>
+            
+            <XTerm className="container-fluid" ref={this.xtermRef}/>
         );
     }
 }
